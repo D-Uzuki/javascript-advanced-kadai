fba6c81ce01ff4028c8179990cce1a079f26a8b2
Daisuke-U <blueskyjapannature@yahoo.co.jp>
2025-02-17 12:32:30 +0900

    Add files via upload

--- kadai_tutorial_output/main.js
+++ kadai_tutorial_output/main.js
@@ -14,24 +14,24 @@ const truetyped = document.getElementById('truetyped');
 
 // 複数のテキストを格納する配列
 const textLists = [
-  'Hello World','This is my App','How are you?',
-  'Today is sunny','I love JavaScript!','Good morning',
-  'I am Japanese','Let it be','Samurai',
-  'Typing Game','Information Technology',
-  'I want to be a programmer','What day is today?',
-  'I want to build a web app','Nice to meet you',
-  'Chrome Firefox Edge Safari','machine learning',
-  'Brendan Eich','John Resig','React Vue Angular',
-  'Netscape Communications','undefined null NaN',
-  'Thank you very much','Google Apple Facebook Amazon',
-  'ECMAScript','console.log','for while if switch',
-  'var let const','Windows Mac Linux iOS Android',
+  'Hello World', 'This is my App', 'How are you?',
+  'Today is sunny', 'I love JavaScript!', 'Good morning',
+  'I am Japanese', 'Let it be', 'Samurai',
+  'Typing Game', 'Information Technology',
+  'I want to be a programmer', 'What day is today?',
+  'I want to build a web app', 'Nice to meet you',
+  'Chrome Firefox Edge Safari', 'machine learning',
+  'Brendan Eich', 'John Resig', 'React Vue Angular',
+  'Netscape Communications', 'undefined null NaN',
+  'Thank you very much', 'Google Apple Facebook Amazon',
+  'ECMAScript', 'console.log', 'for while if switch',
+  'var let const', 'Windows Mac Linux iOS Android',
   'programming'
 ];
 
 // ランダムなテキストを表示
 const createText = () => {
-  
+
   // 正タイプした文字列をクリア
   typed = '';
   typedfield.textContent = typed;
@@ -48,17 +48,17 @@ const createText = () => {
 const keyPress = e => {
 
   // 誤タイプの場合
-  if(e.key !== untyped.substring(0, 1)) {    
+  if (e.key !== untyped.substring(0, 1)) {
     wrap.classList.add('mistyped');
-     // 100ms後に背景色を元に戻す
-     setTimeout(() => {
+    // 100ms後に背景色を元に戻す
+    setTimeout(() => {
       wrap.classList.remove('mistyped');
     }, 100);
     return;
   }
 
   // 正タイプの場合
-  
+
   // スコアのインクリメント
   score++;
   wrap.classList.remove('mistyped');
@@ -67,52 +67,56 @@ const keyPress = e => {
   typedfield.textContent = typed;
   untypedfield.textContent = untyped;
 
-  
+
   // タイマー部分のHTML要素（p要素）を取得する
   let truetypedcount = truetyped.textContent;
-  typedcount++; 
+  typedcount++;
   truetyped.textContent = typedcount;
 
-   // テキストがなくなったら新しいテキストを表示
-   if(untyped === '') {
+  // テキストがなくなったら新しいテキストを表示
+  if (untyped === '') {
     createText();
-   }
+  }
 };
 
 
 // タイピングスキルのランクを判定
 const rankCheck = score => {
 
-  
+
   // テキストを格納する変数を作る
   let text = '';
- 
+
   // スコアに応じて異なるメッセージを変数textに格納する
-  if(score < 100) {
+  if (score < 100) {
     text = `あなたのランクはCです。\nBランクまであと${100 - score}文字です。`;
-  } else if(score < 200) {
-    text = `あなたのランクはBです。\nAランクまであと${200 - score}文字です。`;    
-  } else if(score < 300) {
-    text = `あなたのランクはAです。\nSランクまであと${300 - score}文字です。`;    
-  } else if(score >= 300) {
-    text = `あなたのランクはSです。\nおめでとうございます!`;    
+  } else if (score < 200) {
+    text = `あなたのランクはBです。\nAランクまであと${200 - score}文字です。`;
+  } else if (score < 300) {
+    text = `あなたのランクはAです。\nSランクまであと${300 - score}文字です。`;
+  } else if (score >= 300) {
+    text = `あなたのランクはSです。\nおめでとうございます!`;
   }
- 
+
+
   // 生成したメッセージと一緒に文字列を返す
   return `${score}文字打てました!\n${text}\n【OK】リトライ / 【キャンセル】終了`;
-  
+
 };
 
 // ゲームを終了
 const gameOver = id => {
   clearInterval(id);
-
-  const result = confirm(rankCheck(score));
-
-  // OKボタンをクリックされたらリロードする
-  if(result == true) {
-    window.location.reload();
-  }
+  untypedfield.textContent = 'タイムアップ！';
+  
+  setTimeout(() => {
+    const result = confirm(rankCheck(score));
+    // OKボタンをクリックされたらリロードする
+    if (result == true) {
+      window.location.reload();
+    }
+  }, 10);
+  
 };
 
 // カウントダウンタイマー
@@ -128,8 +132,9 @@ const timer = () => {
     count.textContent = time;
 
     // カウントが0になったらタイマーを停止する
-    if(time <= 0) {
+    if (time <= 0) {
       gameOver(id);
+
     }
   }, 1000);
 };
@@ -152,3 +157,4 @@ start.addEventListener('click', () => {
 });
 
 untypedfield.textContent = 'スタートボタンで開始';
+
